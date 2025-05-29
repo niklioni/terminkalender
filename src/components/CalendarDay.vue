ce5b53ef5e283a7415d6305f9cf7dd211727fe62
niklioni <niklioni@outlook.de>
2025-05-29 09:51:19 +0200

    Niklas

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,23 @@
+<script>
+import CalendarEvent from "@/components/CalendarEvent.vue";
+export default {
+  name: "CalendarDay",
+  components: { CalendarEvent },
+  props: {
+    day: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-header text-center" role="button">
+      <strong>{{ day.fullName }}</strong>
+    </div>
+    <div class="card-body">
+      <CalendarEvent />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,22 @@
+<script>
+import CalendarDay from "@/components/CalendarDay.vue";
+import Store from "../store";
+
+export default {
+  name: "CalendarWeek",
+  components: { CalendarDay },
+  data() {
+    return { calendarWeekData: Store.state.calendarWeekData };
+  },
+};
+</script>
+<template>
+  <div id="calender-week">
+    <div class="card-group">
+      <CalendarDay v-for="day in calendarWeekData" :key="day.id" :day="day" />
+    </div>
+  </div>
+  <!-- Ende: Template für die Calendar-Week-Component -->
+</template>
+
+<style scoped></style>
